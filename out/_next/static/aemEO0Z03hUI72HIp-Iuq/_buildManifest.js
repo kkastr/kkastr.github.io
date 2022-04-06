3ab34be87b4173592d88df89e497c75e8e63c397
kkastr <dinokastritis@gmail.com>
2022-04-06 19:13:47 -0400

    Deploy Next.js to gh-pages

---
+++ out/_next/static/aemEO0Z03hUI72HIp-Iuq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-3e3efd3a194d292c.js"],"/_error":["static/chunks/pages/_error-5b31083fea36d153.js"],"/about":["static/chunks/pages/about-d4b80f69aafb76f2.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
