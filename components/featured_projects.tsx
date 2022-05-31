c7cff8e0eb6ef95a039495162f83394f442a261d
kkastr <dinokastritis@gmail.com>
2022-05-30 21:35:42 -0400

    changed order of projects so that reddit sentiment is first + minor rewording.

--- components/featured_projects.tsx
+++ components/featured_projects.tsx
@@ -1,39 +1,37 @@
 import NextLink from 'next/link'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
-import { Container, Heading, Link,} from '@chakra-ui/react'
+import { Container, Heading, Link, } from '@chakra-ui/react'
 
 import FeaturedItem from './featured_item'
 
 export default function ProjectSection() {
 
-    const section_name='Featured Projects'
-    const gscholarLink='https://scholar.google.ca/citations?hl=en&user=lVNtC6cAAAAJ'
+    const section_name = 'Featured Projects'
+    const gscholarLink = 'https://scholar.google.ca/citations?hl=en&user=lVNtC6cAAAAJ'
 
     return (
         <Container mt={10} maxW='4xl'>
             <Heading mb={10}> {section_name} </Heading>
 
             <FeaturedItem
+                title='Sentiment Analysis'
+                longDescription={`Scrape data from reddit using the reddit api and analyze the comment sentiment. Obtain information such as the sentiment breakdown, average sentiment per post, and more.`}
+                contentLink='https://github.com/kkastr/reddit-sentiment/'
+                previewImage={'./images/sentiment_percent_per_sub.png'}
+                imageLocationLeft={true}
+                imgHeight={346}
+                imgAspect={1.32075471698}
+            />
+
+            <FeaturedItem
                 title='Stock predictions'
                 longDescription={`Generate predictions for the closing value of securities by using Long Short Term Memory (LSTM) neural networks to model the time series of the stock price.`}
                 contentLink='https://github.com/kkastr/stock-price-predictions/'
                 previewImage={'./images/MSFT_pred.png'}
-                imageLocationLeft={true}
-                imgHeight={242}
-                imgAspect = {2.32365145228}
-            />
-
-
-            <FeaturedItem
-                title='Sentiment Analysis'
-                longDescription={`Scrape data from reddit using the reddit api and conduct sentiment analysis on the comments. Obtain information such as the average sentiment per post, the best time of day to post to maximize score, and more.`}
-                contentLink='https://github.com/kkastr/reddit-sentiment/'
-                previewImage={'./images/sentiment_percent_per_sub.png'}
                 imageLocationLeft={false}
-                imgHeight={346}
-                imgAspect= {1.32075471698}
+                imgHeight={242}
+                imgAspect={2.32365145228}
             />
-
 
             <FeaturedItem
                 title='Brownian dynamics'
@@ -42,14 +40,14 @@ export default function ProjectSection() {
                 previewImage={'./images/bd_anim.gif'}
                 imageLocationLeft={true}
                 imgHeight={346}
-                imgAspect= {1}
+                imgAspect={1}
             />
 
 
-            <Container display={{md: 'flex'}} centerContent>
+            <Container display={{ md: 'flex' }} centerContent>
                 <Heading> Research </Heading>
-                <NextLink href={gscholarLink }passHref>
-                <Link>Google Scholar<ExternalLinkIcon mx='2px'/> </Link>
+                <NextLink href={gscholarLink} passHref>
+                    <Link>Google Scholar<ExternalLinkIcon mx='2px' /> </Link>
                 </NextLink>
             </Container>
 
