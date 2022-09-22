b264df37e87f570c2b870558346f939982ebe00b
kkastr <dinokastritis@gmail.com>
2022-09-22 15:38:13 -0400

    reworded bio and updated resume to most recent version

--- components/bio_section.tsx
+++ components/bio_section.tsx
@@ -7,7 +7,7 @@ import {
 
 export default function BioSection() {
     const section_name = 'Bio'
-    const description = `Konstantinos is a physicist and programmer with a passion for
+    const description = `Konstantinos is a physicist turned data scientist with a passion for
                        learning and problem solving. He likes exploring any data he can get his hands on and using code to gain insight into real-life problems.`
 
     const resumepdf = './kastritis_resume.pdf'
